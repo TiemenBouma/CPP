da17c7f1e63a26f08b49eba62810542ce214a572
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-11-29 08:13:53 +0100

    added spaces in cout

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -4,14 +4,14 @@ int	main(int argc, char **argv) {
 	std::string str;
 
 	if (argc == 1) {
-		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 1;
 	}
 	for (int i = 1; i < argc; i++) {
 		str = argv[i];
 		for (size_t j = 0; j < str.length(); j++) {
-			std::cout<<(char)toupper(str[j]);
+			std::cout << (char)toupper(str[j]);
 		}
 	}
-	std::cout<<std::endl;
+	std::cout << std::endl;
 }
