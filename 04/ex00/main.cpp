6c31c0fe2b6f9564732702ca5a04389fbeec6e15
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-12-20 09:26:39 +0100

    Updated CC in makefile

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: tbouma <tbouma@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/13 10:00:38 by tbouma            #+#    #+#             */
-/*   Updated: 2022/12/14 09:05:31 by tbouma           ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: tbouma <tbouma@student.42.fr>                +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/12/13 10:00:38 by tbouma        #+#    #+#                 */
+/*   Updated: 2022/12/20 09:16:51 by tbouma        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,21 @@
 #include "Cat.hpp"
 #include "WrongCat.hpp"
 
-// int main()
-// {
-// }
 int main()
 {
-	// const Animal* meta = new Animal();
-	// const Animal* dog_ptr = new Dog();
-	// const Animal* cat_ptr = new Cat();
-	// std::cout << dog_ptr->getType() << " " << std::endl;
-	// std::cout << cat_ptr->getType() << " " << std::endl;
-	// cat_ptr->makeSound(); //will output the cat sound!
-	// dog_ptr->makeSound();
-	// meta->makeSound();
+	const Animal* meta1 = new Animal();
+	const Animal* dog_ptr1 = new Dog();
+	const Animal* cat_ptr1 = new Cat();
+	std::cout << dog_ptr1->getType() << " " << std::endl;
+	std::cout << cat_ptr1->getType() << " " << std::endl;
+	cat_ptr1->makeSound(); //will output the cat sound!
+	dog_ptr1->makeSound();
+	meta1->makeSound();
 
-	// delete meta;
-	// delete dog_ptr;
-	// delete cat_ptr;
+	delete meta1;
+	delete dog_ptr1;
+	delete cat_ptr1;
+	//-------------------------------------
 	const WrongAnimal* meta = new WrongAnimal();
 	//const WrongAnimal* dog_ptr = new Dog();
 	const WrongAnimal* cat_ptr = new WrongCat();
