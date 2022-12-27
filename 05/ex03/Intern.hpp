464cd27b54c7c5627b4cddfaf42f8caba165b9f5
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-12-27 13:57:33 +0100

    removed unused function declerations

--- 05/ex03/Intern.hpp
+++ 05/ex03/Intern.hpp
@@ -14,11 +14,6 @@ public:
 	Intern&	operator=( const Intern& other );
 
 	AForm	*makeForm(std::string form, std::string target);
-	AForm	*createShrubberyCreationForm( std::string target );
-	AForm	*createRobotomyRequestForm( std::string target );
-	AForm	*createPresidentialPardonForm( std::string target );
-
-
 };
 
 #endif
