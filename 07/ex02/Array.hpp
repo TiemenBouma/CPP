a7bd59eeaa98f43683aa06f0b3c44dc5c831819d
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2023-01-03 09:38:38 +0100

    07

---
+++ 07/ex02/Array.hpp
@@ -0,0 +1,78 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#pragma once
+#include <iostream>
+#include <exception>
+
+template <typename T> 
+class Array
+{
+	private:
+		T *				_array;
+		unsigned int	_size;
+	public:
+		Array() : _array(0), _size(0) {}
+		
+		Array(unsigned int size) {
+			_size = size;
+			_array = new T[size]();
+		}
+
+		Array(const Array &other) {
+			if (other._size == 0) {
+				_size = 0;
+				_array = 0;
+			}
+			else {
+				_size = other._size;
+				_array = new T[_size]();
+				for (size_t i = 0; i < _size; i++) {
+					_array[i] = other._array[i];
+				}
+			}
+		}
+
+		Array operator=(Array &other) {
+			if (_size > 0)
+				delete[] _array;
+			if (other._size == 0) {
+				_size = 0;
+				_array = 0;
+			}
+			else {
+				_size = other._size;
+				_array = new T[_size]();
+				for (size_t i = 0; i < _size; i++) {
+					_array[i] = other._array[i];
+				}
+			}
+			return *this;
+			
+		}
+
+		~Array() {
+			if (_size > 0)
+				delete[] _array;
+		}
+
+		class OutOfBoundException : public std::exception {
+			public:
+				virtual const char* what() const throw(){
+					return "Index is out of bound.";
+				};
+		};
+		
+		T &operator[](size_t i) {
+			if (i >= (this->size())) {
+				throw OutOfBoundException();
+			}
+			return _array[i];
+		}
+
+		unsigned int size() {
+			return (_size);
+		}
+};
+
+#endif
---
+++ 07/ex02/main.cpp
@@ -0,0 +1,91 @@
+#include "Array.hpp"
+#include <iostream>
+
+#define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;
+//     return 0;
+// }
+int main()
+{
+	//-----------Int Array
+	Array<int> int_array(5);
+	int_array[0] = 1;
+	int_array[1] = 2;
+	int_array[2] = 3;
+	int_array[3] = 4;
+	int_array[4] = 5;
+	try {
+		int_array[5] = 6;
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		std::cout << int_array[5] << std::endl;
+	}
+	catch (std::exception &e){
+		std::cout << e.what() << std::endl;
+	}
+	for (size_t i = 0; i < int_array.size(); i++) {
+		std::cout << int_array[i] << std::endl;
+	}
+//------------------String array
+	Array<std::string> str(2);
+	str[0] = "Boe!";
+	str[1] = "Whoof";
+	for (size_t i = 0; i < str.size(); i++) {
+		std::cout << str[i] << std::endl;
+	}
+	try {
+		std::cout << str[2] << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
+}
