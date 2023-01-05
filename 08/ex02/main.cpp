797cb8d12d6bc32efdc2e81fea3f4dbe41bcc4c4
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2023-01-05 13:03:45 +0100

    main

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -25,15 +25,23 @@ int main()
 	//[...]
 	mstack.push('x');
 	MutantStack<char> mstack2 = mstack;
+	MutantStack<char> mstack3(mstack);
 	std::cout << mstack2.size() << std::endl;
+	std::cout << mstack3.size() << std::endl;
 	MutantStack<char>::iterator it = mstack.begin();
 	MutantStack<char>::iterator ite = mstack.end();
+	MutantStack<char>::iterator it2 = mstack2.begin();
+	MutantStack<char>::iterator ite2 = mstack2.end();
+	MutantStack<char>::iterator it3 = mstack3.begin();
+	MutantStack<char>::iterator ite3 = mstack3.end();
 	++it;
 	--it;
-	while (it != ite)
+	while (it != ite || it2 != ite2 || it3 != ite3)
 	{
-		std::cout << *it << std::endl;
+		std::cout << *it << " " << *it2 << " " << *it3 << std::endl;
 		++it;
+		++it2;
+		++it3;
 	}
 	std::stack<char> s(mstack);
 	return 0;
